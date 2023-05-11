4f732949a6372d53552801ecc09f289b92291c66
bdeon <bdeon@student.21-school.ru>
2023-05-11 14:17:02 +0700

    need fix schedule

--- src/dags/add/settings.py
+++ src/dags/add/settings.py
@@ -1,8 +1,7 @@
 text_filter = '"data engineer" OR "ml engineer" OR "etl developer" OR "bi engineer"'
 
 pagination_folder = '/opt/airflow/data'
-# vacancies_folder = '/opt/airflow/data/vac'
-vacancies_folder = '../data/vac'
+vacancies_folder = '/opt/airflow/data/vac'
 
 username = 'airflow'
 password = 'airflow'
